f863ef19acb57e43b12b0131d1f6f3477167fa7a
Alexander <korennoy21@gmail.com>
2024-04-06 11:08:27 +0300

    docstring

--- str_func.py
+++ str_func.py
@@ -1,2 +1,15 @@
 def convert_to_uppercase(input_str):
     return input_str.upper()
+
+
+def convert_to_uppercase(input_str):
+    """
+    Convert input string to uppercase.
+
+    Args:
+        input_str (str): Input string to convert.
+
+    Returns:
+        str: Uppercase version of the input string.
+    """
+    return input_str.upper()
