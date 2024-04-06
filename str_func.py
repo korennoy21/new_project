abeb09bafaced64cbdc775ebc57f65e597f9747a
korennoy21 <165386492+korennoy21@users.noreply.github.com>
2024-04-06 14:07:26 +0300

    Update str_func.py

--- str_func.py
+++ str_func.py
@@ -3,3 +3,14 @@ def convert_to_uppercase(input_str):
 
 
 
+def convert_to_uppercase(input_str):
+    """
+    Convert input string to uppercase.
+    
+    Args:
+        input_str (str): Input string to convert.
+    
+    Returns:
+        str: Uppercase version of the input string.
+    """
+    return input_str.upper()
