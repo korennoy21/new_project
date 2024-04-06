970849cbad3ee09ed690332f21eba836a8fe3a5b
Alexander <korennoy21@gmail.com>
2024-04-06 11:01:26 +0300

    Revert "docstring.py"
    This reverts commit 0c24ab694ebd34be679037e4463dac248c60d492.

--- str_func.py
+++ str_func.py
@@ -1,18 +1,2 @@
 def convert_to_uppercase(input_str):
     return input_str.upper()
-
-
-
-def convert_first_letters_uppercase(input_str):
-    """
-    Convert the first letter of each word in the input string to uppercase.
-    
-    Args:
-        input_str (str): Input string to process.
-    
-    Returns:
-        str: Input string with the first letter of each word in uppercase.
-    """
-    words = input_str.split()
-    capitalized_words = [word.capitalize() for word in words]
-    return ' '.join(capitalized_words)
